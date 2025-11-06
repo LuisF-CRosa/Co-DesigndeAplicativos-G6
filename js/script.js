6808029af8f4dfb38a3979de87d49ba54789eff6
Joao <joaopdbm@al.insper.edu.br>
2025-11-06 16:13:24 -0300

    js parcial e acerto da referencia

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    anunciar=document.querySelector('a')
+    info=document.querySelectorAll('input').value
+
+    anunciar.addEventListener('click',function(event){
+
+
+        dados={'produto':info[0],'preco':info[1],'quantidade':info[2]}
+        localStorage.setItem('anuncio',dados)
+        
+
+    })
 
 })
