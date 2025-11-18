9ec67c9902da253f7297d1bf6ea029d22195c38f
Luan <luanmfp@al.insper.edu.br>
2025-11-18 11:24:44 -0300

    java bolsa

--- js/Bolsa.js
+++ js/Bolsa.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Encontra todos os elementos e os coloca numa especie de lista
     const elementos = document.querySelectorAll('.elemento')
+    const elementos2 = document.querySelectorAll('.elemento2')
     //encontra a imagem que sera modifica
     const bolsa = document.querySelector('.bolsa')
     // muda a cor da borda da imagem inicial por verde
@@ -14,6 +15,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 bolsa.src = 'img/Grafico_produto1.png'
                 elementos[0].style.borderColor = 'green'
+                elementos2[1].innerHTML = 'Valor Médio: 2,1134'
+                elementos2[2].innerHTML = 'Valor Mínimo: 2,0354'
             })
         }
         else if(elemente == elementos[1]){
@@ -23,6 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 bolsa.src = 'img/grafico_ban.jpeg'
                 elementos[1].style.borderColor = 'green'
+                elementos2[1].innerHTML = 'Valor Médio: 2,1364'
+                elementos2[2].innerHTML = 'Valor Mínimo: 2,0236'
             })
         }
         else if(elemente == elementos[2]){
@@ -32,6 +37,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 bolsa.src = 'img/Grafico_produto.png'
                 elementos[2].style.borderColor = 'green'
+                elementos2[1].innerHTML = 'Valor Médio: 2,2534'
+                elementos2[2].innerHTML = 'Valor Mínimo: 2,0201'
             })
         }
         else if(elemente == elementos[3]){
@@ -41,6 +48,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 bolsa.src = 'img/Grafico_produto2.png'
                 elementos[3].style.borderColor = 'green'
+                elementos2[1].innerHTML = 'Valor Médio: 2,2839'
+                elementos2[2].innerHTML = 'Valor Mínimo: 2,0201'
             })
         }
         else if(elemente == elementos[4]){
@@ -50,6 +59,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 bolsa.src = 'img/Grafico_4.png'
                 elementos[4].style.borderColor = 'green'
+                elementos2[1].innerHTML = 'Valor Médio: 2,1965'
+                elementos2[2].innerHTML = 'Valor Mínimo: 1,993'
             })
         }
     }})
